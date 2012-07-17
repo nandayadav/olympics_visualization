e758e7d3587e584c88e9fe30e232083c4744c495
Nanda Yadav <nandayadav@gmail.com>
2012-07-17 11:04:37 -0600

    more data, some simple collision detected added

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require 'active_support/all'
 class App < Sinatra::Base
   enable :logging
   use Rack::Session::Cookie
-  register Barista::Integration::Sinatra
+  #register Barista::Integration::Sinatra
   get '/' do
     File.read(File.join('public', 'paper.html'))
   end
@@ -15,14 +15,15 @@ class App < Sinatra::Base
   get '/countries' do
     content_type :json
     countries = []
-    CSV.foreach("2008.csv") do |line|
+    year = params[:year] || 2004
+    CSV.foreach("#{year}.csv") do |line|
       country = {}
       country['name'] = line[0]
       country['gold'] = line[1].to_i
       country['silver'] = line[2].to_i
       country['bronze'] = line[3].to_i
-      country['gdp'] = line[5].to_i
-      country['gdp'] = rand(5000) if line[4] == "0"
+      country['gdp'] = line[4].to_i
+      country['gdp'] = rand(5000) if line[4] == "0" #TODO: fix missing GDP data
       countries << country
     end
     countries.to_json
